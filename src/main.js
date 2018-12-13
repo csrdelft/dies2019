ebc1f912381fc1244bbc87e04e886a0f2d62ce8b
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-13 23:24:37 +0100

    IE fix, format text

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Player from './Player.vue'
+import detectIE from "./detect-ie";
 
+// Laad stylesheet (wordt in productie netjes in een los bestand gezet)
 import './assets/style.scss'
 
 Vue.config.productionTip = false
@@ -8,3 +10,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(Player),
 }).$mount('#player')
+
+// Schrale IE/Edge ondersteund geen svg mix-blend-mode
+if (detectIE() !== false) {
+  document.getElementById('vinyl-svg').style.opacity = "0";
+}
