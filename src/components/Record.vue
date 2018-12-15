ce415c1a687ba919264b9b6db4c1f5fa84cb1d93
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-15 16:55:57 +0100

    Betere svg, fix versie voor de platen

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,13 +1,13 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-         viewBox="0 0 3580.8777 3512.5046">
-        <defs id="defs4686">
-            <clipPath id="vinyl">
+         viewBox="0 0 300 307">
+        <defs>
+            <clipPath id="vinyl-record">
                 <!-- clip path in de vorm van een lp. -->
-                <path d="M 1790.6447,240.95477 A 1515.6484,1515.6484 0 0 0 274.99581,1756.6036 1515.6484,1515.6484 0 0 0 1790.6447,3272.2525 1515.6484,1515.6484 0 0 0 3306.2936,1756.6036 1515.6484,1515.6484 0 0 0 1790.6447,240.95477 Z m 0.9699,1475.18323 a 40.779928,40.779928 0 0 1 40.7789,40.7803 40.779928,40.779928 0 0 1 -40.7789,40.7806 40.779928,40.779928 0 0 1 -40.7807,-40.7806 40.779928,40.779928 0 0 1 40.7807,-40.7803 z"></path>
+                <path d="M 150 1.5 A 149 152 0 0 0 1 153.5 A 149 152 0 0 0 150 305.5 A 149 152 0 0 0 299 153.5 A 149 152 0 0 0 150 1.5 z M 145 150 A 4 4 0 0 1 149 154 A 4 4 0 0 1 145 158 A 4 4 0 0 1 141 154 A 4 4 0 0 1 145 150 z"></path>
             </clipPath>
         </defs>
-        <image xlink:href="images/vinyl-achtergrond.jpg" clip-path="url(#vinyl)" x="271.29498" y="237.49956" width="3040" height="3038"></image>
+        <image xlink:href="images/vinyl-record.jpg" clip-path="url(#vinyl-record)" x="0" y="0" width="300" height="307"></image>
     </svg>
 </template>
 
@@ -21,9 +21,9 @@
     svg {
         top: 350px;
         opacity: 0;
-        width: 350px;
-        height: 350px;
+        width: 300px;
+        height: 307px;
         position: relative;
-        margin-left: calc(50% - 175px);
+        margin: 30px 30px 30px calc(50% - 150px);
     }
 </style>
