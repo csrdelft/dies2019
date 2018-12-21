c67ad20c0f470219c01a8c6e01a086efcd4540b5
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-21 16:54:08 +0100

    Maak clip-path van record een ellipse ipv een ingewikkeld path

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -4,7 +4,7 @@
         <defs>
             <clipPath id="vinyl-record">
                 <!-- clip path in de vorm van een lp. -->
-                <path d="M 150 1.5 A 149 152 0 0 0 1 153.5 A 149 152 0 0 0 150 305.5 A 149 152 0 0 0 299 153.5 A 149 152 0 0 0 150 1.5 z M 145 150 A 4 4 0 0 1 149 154 A 4 4 0 0 1 145 158 A 4 4 0 0 1 141 154 A 4 4 0 0 1 145 150 z"></path>
+                <ellipse cx="151" cy="154" ry="152" rx="148"></ellipse>
             </clipPath>
         </defs>
         <image xlink:href="images/vinyl-record.jpg" clip-path="url(#vinyl-record)" x="0" y="0" width="300" height="307"></image>
